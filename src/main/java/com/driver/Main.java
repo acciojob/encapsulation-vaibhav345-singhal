7c807574a8cf5223228d123c1ba6a823be9fd945
vaibhav <ankitkumar35546567@gmail.com>
2023-02-08 10:21:43 +0530

    data hiding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        obj.name = "available";
+//        System.out.println(obj.name);
+        //Error: name has private access in com.driver.RWOnly
+
+        obj.setter("available");
+
+        System.out.println(obj.getter());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public void setter(String val){
+        this.name = val;
+    }
+
+    public String getter(){
+        return this.name;
+    }
 }
